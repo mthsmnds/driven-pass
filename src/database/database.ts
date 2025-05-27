d4825337cf09bc8839af3d96a016f28a38a5fb28
Matheus Mendes <mathmendes.f@gmail.com>
2025-05-27 16:57:11 -0300

    prisma integrated

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,10 +1,4 @@
-import pg from "pg";
-import dotenv from "dotenv";
-dotenv.config();
+import { PrismaClient } from "../generated/prisma"
+const prisma = new PrismaClient();
 
-const {Pool} = pg;
-const db = new Pool({
-    connectionString: process.env.DATABASE_URL
-})
-
-export default db;
+export default prisma;
--- src/repositories/userRepo.ts
+++ src/repositories/userRepo.ts
@@ -1,18 +1,21 @@
 import { InsertUser, User } from "../protocols/protocols";
-import db from "../database/database";
-
+import prisma from "../database/database";
 
 export async function findEmail(email: string){
-    const result = await db.query<User>(`SELECT * FROM users WHERE email = $1`,[email]);
-    return result.rows[0];
+    const result = await prisma.users.findUnique({
+        where:{email}
+    })
+    return result;
 }
 
 export async function signUpRepo(userData: InsertUser){
     const {name, email, password} = userData;
-    const result = await db.query<User>(`
-        INSERT INTO users (name, email, password)
-        VALUES ($1, $2, $3)
-        RETURNING *;
-        `,[name, email, password]);
-    return result.rows[0];
+    const newUser = await prisma.users.create({
+        data:{
+            name,
+            email,
+            password
+        }
+    });
+    return newUser;
 }
